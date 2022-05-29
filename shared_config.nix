1a4e6c787368530141ecc418fd578270d8783c64
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-05-29 12:24:40 +0200

    Telegram Desktop, Signal Deskop

--- shared_config.nix
+++ shared_config.nix
@@ -242,6 +242,8 @@ in
     mosh
     frp
     tmux
+    tdesktop
+    signal-desktop
   ];
 
   # Allow the user run a program to poweroff the system. (Copied and adapted from https://discourse.nixos.org/t/how-to-configure-nixos-to-allow-a-program-to-trigger-shutdown/11582)
