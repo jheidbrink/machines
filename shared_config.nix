c7854ed6da30c188f3976e0a240e8f0691a3f264
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-06-01 14:00:44 +0200

    socat

--- shared_config.nix
+++ shared_config.nix
@@ -244,6 +244,7 @@ in
     tmux
     tdesktop
     signal-desktop
+    socat
   ];
 
   # Allow the user run a program to poweroff the system. (Copied and adapted from https://discourse.nixos.org/t/how-to-configure-nixos-to-allow-a-program-to-trigger-shutdown/11582)
