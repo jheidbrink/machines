1165b56166703998a76c31208030392590b29023
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-08-16 21:48:05 +0200

    gparted
    doesn't work: Error executing command as another user: No authentication agent found.

--- shared_config.nix
+++ shared_config.nix
@@ -261,6 +261,7 @@ in
     speechd # for spd-say as notification in shell scripts
     haskellPackages.git-annex
     qemu
+    gparted
   ];
 
   # Allow the user run a program to poweroff the system. (Copied and adapted from https://discourse.nixos.org/t/how-to-configure-nixos-to-allow-a-program-to-trigger-shutdown/11582)
