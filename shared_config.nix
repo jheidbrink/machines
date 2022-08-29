a898774c088a77d9b80c0ab9ac9daf9531450931
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-08-29 18:14:13 +0200

    redshift

--- shared_config.nix
+++ shared_config.nix
@@ -281,6 +281,7 @@ in
     easyeffects
     helvum
     fasd
+    redshift
   ];
 
   # Allow the user run a program to poweroff the system. (Copied and adapted from https://discourse.nixos.org/t/how-to-configure-nixos-to-allow-a-program-to-trigger-shutdown/11582)
