3f710f75858b3edbb97c8678c86bd3baf7a167a2
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-10-05 09:12:27 +0200

    retiolum

---
+++ retiolum.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+
+  netname = "retiolum";
+  cfg = config.networking.retiolum;
+
+  retiolum = pkgs.fetchgit {
+    url = "https://github.com/krebs/retiolum.git";
+    rev = "231fb4c49fba27446fb89b4e189b2f501c969d55";
+    sha256 = "sha256-BPEmrPai+kIsXXJZstKhNUXIh/peQmg/xwBOYxViMRk=";
+  };
+
+in {
+  options = {
+    networking.retiolum.ipv4 = mkOption {
+      type = types.str;
+      description = ''
+        own ipv4 address
+      '';
+    };
+    networking.retiolum.ipv6 = mkOption {
+      type = types.str;
+      description = ''
+        own ipv6 address
+      '';
+    };
+    networking.retiolum.nodename = mkOption {
+      type = types.str;
+      default = config.networking.hostName;
+      description = ''
+        tinc network name
+      '';
+    };
+  };
+
+  config = {
+
+    services.tinc.networks.${netname} = {
+      name = cfg.nodename;
+      extraConfig = ''
+        LocalDiscovery = yes
+        AutoConnect = no
+        ConnectTo = prism
+      '';
+    };
+    systemd.services."tinc.${netname}" = {
+      preStart = ''
+        cp -R ${retiolum}/hosts /etc/tinc/retiolum/ || true
+      '';
+    };
+
+    networking.extraHosts = builtins.readFile (toString "${retiolum}/etc.hosts");
+
+    environment.systemPackages = [ config.services.tinc.networks.${netname}.package ];
+
+    networking.firewall.allowedTCPPorts = [ 655 ];
+    networking.firewall.allowedUDPPorts = [ 655 ];
+    #services.netdata.portcheck.checks.tinc.port = 655;
+
+    systemd.network.enable = true;
+    systemd.network.networks = {
+      "${netname}".extraConfig = ''
+        [Match]
+        Name = tinc.${netname}
+        [Network]
+        Address=${cfg.ipv4}/12
+        Address=${cfg.ipv6}/16
+      '';
+    };
+  };
+}
