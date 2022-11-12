02d06cd40824fd06444ad74e5852ac6bf001fd24
Jan Heidbrink <jan.heidbrink@posteo.de>
2022-11-12 12:47:27 +0100

    nixos-generators

--- shared_config.nix
+++ shared_config.nix
@@ -194,6 +194,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    nixos-generators
     wget
     firefox
     tree
