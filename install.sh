a63be004802eb7467836d6089d869dc358f3cfe7
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-01-16 12:12:26 +0100

    Sync less files

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -eu
 
 host=$1
 
-rsync --recursive --exclude=.mypy_cache ./ /etc/nixos/
+rsync --recursive --exclude=.mypy_cache --exclude=.git --exclude=.*.swp ./ /etc/nixos/
 ln -sf configuration-"${host}".nix /etc/nixos/configuration.nix
 nixos-rebuild switch
