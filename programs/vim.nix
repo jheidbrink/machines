fcbe6e2c9d35b45b9b6ebd703b5fdf7b1d584129
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-02-21 10:32:57 +0100

    Add Vim colorschemes Noctu and Dim

--- programs/vim.nix
+++ programs/vim.nix
@@ -24,6 +24,28 @@ vim-colors-github = pkgs.vimUtils.buildVimPluginFrom2Nix {
     };
   };
 
+  vim-colors-dim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "vim-colors-dim";
+    version = "0.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "jeffkreeftmeijer";
+      repo = "vim-dim";
+      rev = "8320a40f12cf89295afc4f13eb10159f29c43777";
+      sha256 = "sha256-sDt3gvf+/8OQ0e0W6+IinONQZ9HiUKTbr+RZ2CfJ3FY=";
+    };
+  };
+
+  vim-colors-noctu = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "vim-colors-noctu";
+    version = "0.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "noahfrederick";
+      repo = "vim-noctu";
+      rev = "de2ff9855bccd72cd9ff3082bc89e4a4f36ea4fe";
+      sha256 = "sha256-fiMYfRlm/KiMQybL97RcWy3Y+0qim6kl3ZkBvCuv4ZM=";
+    };
+  };
+
 in
 
 {
@@ -50,6 +72,8 @@ in
           tagbar
           vim-colors-ericbn-solarized
           vim-colors-github
+          vim-colors-dim
+          vim-colors-noctu
         ];
       };
       customRC = builtins.readFile ../dotfiles/init.vim;
