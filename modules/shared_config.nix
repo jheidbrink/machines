6f0e2bf33f153db2937acc3cd8899dcb8b8b4157
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-05-31 07:56:49 +0200

    Install dracut for lsinitrd

--- modules/shared_config.nix
+++ modules/shared_config.nix
@@ -2,6 +2,13 @@
 let
   programs = (import ../programs/programs.nix) { inherit pkgs lib; };
 
+  nixpkgs2305 = import (
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/0f7f5ca1cdec8dea85bb4fa60378258171d019ad.tar.gz";  # 2023-05-29 nixos-23.05 branch
+      sha256 = "sha256:0cnv56gmw7ina9gfqp02d9k0526rwnwq34jmcfpl92vawx42arvz";
+    }
+  ) {};
+
 in
 {
   nixpkgs.config.allowUnfree = true;
@@ -211,6 +218,7 @@ in
     programs.print256colors
     programs.print_ansi_colors
     programs.alacritty-solarized
+    nixpkgs2305.dracut  # for lsinitrd
   ];
 
   programs.java.enable = true;
