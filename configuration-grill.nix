e2285c955262b4f104187e94be82ab9b3390336c
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-06-03 15:08:26 +0200

    Grill shouldnt sleep

--- configuration-grill.nix
+++ configuration-grill.nix
@@ -60,6 +60,13 @@ in
 
   users.users.jan.extraGroups = [ "docker" ];
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   #home-manager.users.jan = standard_user_hm_config;  // { home.stateVersion = "23.05"; };  # I believe the stateVersion is the version of home-manager that was first installed on that system. Home-manager is not installed on Grill yet, will start with NixOS 23.05
 
   # Open ports in the firewall.
