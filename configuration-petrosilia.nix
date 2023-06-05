e7248d469f6aa8b2c337906d058c644ecefb41cd
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-06-05 14:22:48 +0200

    Upgrade IntelliJ to 2023.1 or so

--- configuration-petrosilia.nix
+++ configuration-petrosilia.nix
@@ -111,7 +111,7 @@ in
 
   environment.systemPackages = [
     pkgs.meld
-    pkgs.jetbrains.idea-ultimate
+    nixpkgs2305.jetbrains.idea-ultimate
     pkgs.pulseaudio  # this gives me pactl but doesn't run pulseaudio
     pkgs.lxqt.lxqt-archiver
     pkgs.pantheon.evince
