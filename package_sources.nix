08314598cd6f921435b1c859cb0ccdf6624f28c5
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-06-08 09:19:06 +0200

    Try to upgrade Petrosilia to 23.05

--- package_sources.nix
+++
@@ -1,6 +0,0 @@
-{
-  nixpkgs2305_source = builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/0f7f5ca1cdec8dea85bb4fa60378258171d019ad.tar.gz";  # 2023-05-29 nixos-23.05 branch
-      sha256 = "sha256:0cnv56gmw7ina9gfqp02d9k0526rwnwq34jmcfpl92vawx42arvz";
-    };
-}
