4ed0d6009e22dec7d0666710e6afbef5658e27a4
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-11-23 08:46:52 +0100

    Disable docker again
    It messes with my firewall rules and enables IP forwarding. Use a VM for
    Docker.

--- configuration-petrosilia.nix
+++ configuration-petrosilia.nix
@@ -153,8 +153,6 @@ in
     * 192.168.0.0/16
   '';
 
-  virtualisation.docker.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
