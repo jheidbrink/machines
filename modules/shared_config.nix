a2c851f8ffa737490ef804fa5b5da6c092caf6f8
Jan Heidbrink <jan.heidbrink@posteo.de>
2023-12-15 12:00:40 +0100

    zbar

--- modules/shared_config.nix
+++ modules/shared_config.nix
@@ -90,7 +90,7 @@ in
     tree
     jq
     curl
-    (python3.withPackages (ps: [ ps.pyyaml ps.ipython ps.numpy ]))  # putting ipython and yaml in the same environment so that I can `import yaml' from an ipython session
+    (python3.withPackages (ps: [ ps.pyyaml ps.ipython ps.numpy ps.requests ]))  # putting ipython and yaml in the same environment so that I can `import yaml' from an ipython session
     python3Packages.argcomplete  # sourced from shell
     python3Packages.virtualenvwrapper  # loaded by my zshrc
     python3Packages.pip-tools
@@ -237,6 +237,7 @@ in
     usbutils
     feh
     diffuse  # diff an arbitrary amount of files
+    zbar
   ];
 
   programs.java.enable = true;
